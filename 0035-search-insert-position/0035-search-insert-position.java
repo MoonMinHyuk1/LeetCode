243ed899bff567d7593f372df9222cc63549268e
MoonMinHyuk1 <102985637+MoonMinHyuk1@users.noreply.github.com>
2025-01-13 10:38:48 +0900

    Time: 0 ms (100%), Space: 42.9 MB (61.1%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int answer = 0;
+        int lt = 0, rt = nums.length - 1;
+
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                lt = mid + 1;
+            } else {
+                rt = mid - 1;
+            }
+        }
+
+        return lt;
+    }
+}
